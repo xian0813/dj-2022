9bd54ac598146f1cde2a693c612caf1718861858
xian0813 <n822813@gmail.com>
2022-01-18 13:23:24 +0800

    v2-5

--- index2-5.js
+++ index2-5.js
@@ -7,5 +7,21 @@ let n5 = '9' ;
 let n6 = 9;
 
 let s1 = '0922255588';
+let s2 = 'abc'
 
-console.log(n5 === n6);
+let s3 = '國文:80';
+
+let s4 = 80;
+let s6 = `您的
+國文
+分數為${80}`;
+
+console.log(s6);
+
+let s5 = '您的\n';
+s5 +='國文\n';
+s5 +='分數為'+s4;
+
+
+
+console.log(s5);
