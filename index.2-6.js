fac196d4cf670dfbe7d33f05b941b72d937e900b
xian0813 <n822813@gmail.com>
2022-01-18 15:15:49 +0800

    v2-6-1

---
+++ index.2-6.js
@@ -0,0 +1,10 @@
+// 算術運算子: +、-、*、/、%、**
+let n1 = 1;
+let n2 = 2;
+let s1 = '1';
+// console.log(typeof n1);
+// console.log(typeof s1);
+console.log(n1 + n2);
+console.log(n1 - n2);
+console.log(n1 * n2);
+console.log(n1 / n2);
