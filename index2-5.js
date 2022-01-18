636720504837be675684a1b4e4bbe6fb8fa9f738
xian0813 <n822813@gmail.com>
2022-01-18 13:55:44 +0800

    v2-5

--- index2-5.js
+++ index2-5.js
@@ -16,12 +16,45 @@ let s6 = `您的
 國文
 分數為${80}`;
 
-console.log(s6);
+// console.log(s6);
 
 let s5 = '您的\n';
 s5 +='國文\n';
 s5 +='分數為'+s4;
 
+// console.log(s5);
 
+// boolean
+// console.log(10 > 5);
+// console.log(10 < 5);
+// console.log(1);
 
-console.log(s5);
+// underfined
+let u;
+// console.log(u);
+
+let u1 = {};
+// console.log(u1.name);
+
+let u2 = function(x,y){
+    return x+y;
+}
+// console.log(u2(3,4));
+
+let nu1 = null;
+// console.log(nu1);
+
+let x1 = 50;
+let x2 = 100;
+let x3 = 80;
+let x4 = 50;
+let x5 = 90;
+console.log(x1);
+//        0  1   2  3  4
+let xx = [50,100,80,50,90];
+// index value
+// console.log(xx[1])
+
+for(let i=0; i < xx.length; i++){
+    console.log(xx[i]);
+}
