dc0485426fb3ae99ab11a96e46390b25e0cfb211
xian0813 <n822813@gmail.com>
2022-01-19 11:49:39 +0800

    v3-7

---
+++ index.3-6.js
@@ -0,0 +1,16 @@
+
+let card = {
+    name:'林宛璇',
+    tel: '0921560420',
+    addr:'台南市安南區安南路33號',
+    '職稱' : '工程師'
+};
+
+// let x = 'name';
+// console.log(card.name);
+
+for(let i in card){
+    // console.log(card[i]);
+    // console.log(typeof i);
+    console.log(cardd[i])
+}
---
+++ index.3-7.js
@@ -0,0 +1,26 @@
+let rows = ['a', 'b', 'c'];
+// console.log(rows[2]);
+
+// for (let row of rows) {
+//   console.log(row);
+// }
+
+// expected output: "a"
+// expected output: "b"
+// expected output: "c"
+let card1=['林宛璇','0921560420','台南市安南區安南路33號','工程師']
+
+for(let i in card1){
+    console.log(card1[i]);
+}
+
+// let card = {
+//     name:'林宛璇',
+//     tel: '0921560420',
+//     addr:'台南市安南區安南路33號',
+//     '職稱' : '工程師'
+// };
+
+// for(let value ofcar1){
+    // console.log(value);
+// }
