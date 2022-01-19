a39df2cc1c3ed142e4f5584de32bc021fd3731d7
xian0813 <n822813@gmail.com>
2022-01-19 09:47:03 +0800

    v3-1

---
+++ index.3-1.js
@@ -0,0 +1,27 @@
+let scroe = 85;
+/*
+if(scroe >= 60){
+    console.log("您的分數是及格的!");
+} else {
+    console.log("您的分數是不及格的!");
+}
+*/
+if(scroe >= 90){
+    console.log("您的分數是A級!");
+} else if(scroe >= 80){
+    console.log("您的分數是B級!");
+} else if(scroe >= 70){
+    console.log("您的分數是C級!");
+} else if(scroe >= 60){
+    console.log("您的分數是D級!")
+} else {
+    console.log("您的分數是不及格的!");
+}
+// let a = '';
+// a += '1,\n';
+// // a = a +1,';
+// a +='2';
+let b = 'ok';
+let a = `1,${b}
+2`;
+console.log(a);
